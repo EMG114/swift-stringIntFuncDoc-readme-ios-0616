1bddbaf3b73d6329fd7aa8bf7e0e652f9e1d116d
EMG114 <egutierrez411@gmail.com>
2016-07-05 16:30:42 -0400

    oh methods

--- Help.playground/Contents.swift
+++ Help.playground/Contents.swift
@@ -4,3 +4,10 @@ name.uppercaseString
 let uppercaseName = name.uppercaseString
 print("\(name)... \(name)... \(uppercaseName)! WAKE UP!")
 // prints "Audrey... Audrey... AUDREY! WAKE UP!")
+
+
+let lowercaseName = name.lowercaseString
+
+let name2 = "sasey johns"
+
+let capitalName = name2.uppercaseString
